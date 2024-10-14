77ae4286e09d1ab60a7525803e0f0d16b3d1a44e
Daniel Santos Maciel <daniel.maciel@gftpromotora.com.br>
2024-10-14 16:51:14 -0300

    create user repository

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,11 +1,20 @@
 import { Body, Controller, Post } from "@nestjs/common";
+import { UserRepository } from "./user.repository";
 
 @Controller('/users')
 export class UserController {
+
+  private userRepository = new UserRepository()
+  
   @Post()
-  async createUsers() {
+  async createUsers(@Body() userData) {
+
+    this.userRepository.save(userData)
+    
     return {
+      data: userData,
       status: 'user created successfully!'
     }
   }
+
 }
