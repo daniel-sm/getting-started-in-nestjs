ae9f1b24b1dff0b727b5fb22ba5a17f07f07b8db
Daniel Santos Maciel <daniel.maciel@gftpromotora.com.br>
2024-10-15 15:03:26 -0300

    create product module with routes, controllers and repositories

---
+++ src/product/product.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { ProductRepository } from "./product.repository";
+
+@Controller('/products')
+export class ProductController {
+
+  constructor(private productRepository: ProductRepository) {}
+  
+  @Post()
+  async createProduct(@Body() productData) {
+    this.productRepository.save(productData)
+
+    return {
+      data: productData,
+      status: "product created successfully!",
+    }
+  }
+
+  @Get()
+  async listProducts() {
+    return this.productRepository.list()
+  }
+}
