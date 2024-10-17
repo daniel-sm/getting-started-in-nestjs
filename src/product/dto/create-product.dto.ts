112774bfdd5376b9ad5c785c7511523aeb5b2aa6
Daniel Santos Maciel <daniel.maciel@gftpromotora.com.br>
2024-10-17 16:45:04 -0300

    add dto validation for the create product route

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,71 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUrl,
+  MaxLength,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
+export class ProductCharacteristicsDTO {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(1000)
+  description: string;
+}
+
+export class ProductImagesDTO {
+  @IsUrl()
+  @IsNotEmpty()
+  url: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(1000)
+  description: string;
+}
+
+export class CreateProductDTO {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  value: number;
+
+  @IsInt()
+  @Min(0)
+  quantityAvaliable: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(1000)
+  description: string;
+
+  @ValidateNested()
+  @IsArray()
+  @ArrayMinSize(3)
+  @Type(() => ProductCharacteristicsDTO)
+  characteristics: ProductCharacteristicsDTO[];
+
+  @ValidateNested()
+  @IsArray()
+  @ArrayMinSize(1)
+  @Type(() => ProductImagesDTO)
+  images: ProductImagesDTO[];
+
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,23 +1,23 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { ProductRepository } from "./product.repository";
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductRepository } from './product.repository';
+import { CreateProductDTO } from './dto/create-product.dto';
 
 @Controller('/products')
 export class ProductController {
-
   constructor(private productRepository: ProductRepository) {}
-  
+
   @Post()
-  async createProduct(@Body() productData) {
-    this.productRepository.save(productData)
+  async createProduct(@Body() productData: CreateProductDTO) {
+    this.productRepository.save(productData);
 
     return {
       data: productData,
-      status: "product created successfully!",
-    }
+      status: 'product created successfully!',
+    };
   }
 
   @Get()
   async listProducts() {
-    return this.productRepository.list()
+    return this.productRepository.list();
   }
 }
